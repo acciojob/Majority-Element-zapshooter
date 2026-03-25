f5099b03a45b68e99bed72a2e804c74ee8dd8f10
zapshooter <92568875+zapshooter@users.noreply.github.com>
2026-03-25 23:58:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your code here
+function majorityElement(nums) {
+  let candidate = null;
+  let count = 0;
+
+  // Boyer-Moore Voting Algorithm
+  for (let num of nums) {
+    if (count === 0) {
+      candidate = num;
+    }
+    count += num === candidate ? 1 : -1;
+  }
+
+  return candidate;
+}
